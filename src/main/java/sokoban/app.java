6728e848dc370172de8457787f081163c1d9b73a
Marco <deliso.marco97@gmail.com>
2024-10-01 17:57:26 +0200

    added new easy exercise LeetCode, need to debug it

--- src/main/java/sokoban/app.java
+++ src/main/java/sokoban/app.java
@@ -1,35 +1,45 @@
 package sokoban;
 
+// 35. Search Insert Position
 public class app {
-    public static void main(String[] args) {
-        int[] nums = {0,1,2,2,3,0,4,2};
-        int val = 2;
+    public static int searchInsert(int[] nums, int target) {
+        int currMin;
+        int currMax;
 
-        int input = nums.length - 1;
-
-        for (int i = 0; i < nums.length; i ++) {
-            while (nums[input] == val) {
-                if (i != input) {
-                    input--;
-                } else {
-                    System.out.println("nums: " + nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7]);
-                    System.out.print(input + 1);
-                    return;
-                }
-                
-            }
-
-            if (nums[i] == val) {
-                nums[i] = nums[input];
-                //nums[input] = val;
-                input--;
-            }
-
-            if (i == input) {
-                System.out.println("nums: " + nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7]);
-                System.out.print(input + 1);
-                return;
-            }
+        // select first split by assigning min and max for binary search
+        if (nums[nums.length - 1] - target > Math.round(nums.length/2)) {
+            // target lays in upper part of subsection
+            currMin = Math.round(nums.length/2);
+            currMax = nums[nums.length - 1];
+        } else {
+            // target lays in lower part of subsection
+            currMin = 0;
+            currMax = Math.round(nums.length/2);
         }
+        
+
+        while (!(nums[currMin] < target && nums[currMin + 1] > target) || !(nums[currMax] < target && nums[currMax + 1] > target)) {
+            
+            if (nums[currMax - Math.round((currMax - currMin) / 2)] > target) {
+                currMax = currMax - Math.round((currMax - currMin) / 2);
+            } else {
+                currMin = currMax - Math.round((currMax - currMin) / 2);
+            }
+
+            if (nums[currMin] == target) return currMin;
+            if (nums[currMax] == target) return currMax;
+        }
+
+        if (nums[currMin] < target && nums[currMin + 1] > target) return currMin + 1;
+        if (nums[currMax] > target && nums[currMax - 1] < target) return currMax - 1;
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 2;
+
+        System.out.print(searchInsert(nums, target));
     }
 }
